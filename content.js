5544aa3928fe97452135515f0442c1a832024d27
rakshithakotilingala9-web <rakshithakotilingala9@gmail.com>
2025-11-19 18:58:42 +0530

    Update content4

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 (() => {
   const ROOT_ID = 'vh-agui-root';
   if (document.getElementById(ROOT_ID)) return;
-
-  // ---------- Utilities ----------
+  // testing //
+  // ---------- Utilities ---------- //
   const qs = (sel, root = document) => root.querySelector(sel);
   const tidy = (s = '') => s.replace(/\xA0/g, ' ').replace(/[ \t]+\n/g, '\n').trim();
   function normalizeUrl() {
